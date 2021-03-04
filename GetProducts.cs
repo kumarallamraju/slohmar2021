24564fb86bfd7127f86e7ff7cb4e00cb9d9b4e84
Kumar Allamraju <kumar.allamraju@gmail.com>
2021-03-03 22:12:41 -0800

    push GetProducts file

---
+++ GetProducts.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using SOH21DryRunFunctionApp.Models;
+using System.Net.Http;
+using System.Threading.Tasks;
+ 
+namespace SOH21DryRunFunctionApp
+{
+    public static class GetProducts
+    {
+        [FunctionName("GetProducts")]
+       public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,ILogger log)
+        {
+            HttpClient _http = new HttpClient();
+            log.LogInformation("Getting Products");
+            var response = await _http.GetAsync("https://serverlessohproduct.trafficmanager.net/api/GetProducts");
+ 
+            if (response == null)
+            {
+                return new NotFoundResult();
+            }
+            else
+            {
+                  var resultContent = await response.Content.ReadAsStringAsync();
+                return new OkObjectResult(resultContent);
+            }
+        }
+    }
+}
